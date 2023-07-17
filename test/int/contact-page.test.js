841e9badc691a5f80eacc511b9c24d6ef1119744
Jonas Persson <jonas@jonaspersson.dev>
2023-07-17 15:08:55 -0700

    Mocha configured to run a single file importing all tests there when npm test is run at the CL

---
+++ test/int/contact-page.test.js
@@ -0,0 +1,8 @@
+/**
+ * @module test-integration-contact-page
+ * Integration tests for contact.html
+ * @author Jonas Persson
+ * /
+*/
+
+module.exports = console.log('contact-page.test.js executing');
---
+++ test/run-all.js
@@ -0,0 +1,3 @@
+
+console.log('run-all.js executing');
+require('./int/contact-page.test.js');
